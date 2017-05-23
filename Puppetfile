60ace75508b664cbe6e92ae9485bd60976813fbc
Thomas Linkin <trlinkin@gmail.com>
2017-05-23 16:28:19 -0400

    Initial commit

---
+++ Puppetfile
@@ -0,0 +1,9 @@
+forge "http://forge.puppetlabs.com"
+
+mod "puppetlabs/inifile",   '1.6.0'
+mod "puppetlabs/stdlib",    '4.17.0'
+mod "puppetlabs/java",      '1.6.0'
+mod "puppetlabs/concat",    '4.0.0'
+mod "rtyler-jenkins",       '1.7.0'
+mod 'puppetlabs-git',       '0.5.0'
+mod 'WhatsARanjit-node_manager', '0.4.2'
---
+++ site/profile/manifests/jenkins.pp
@@ -0,0 +1,31 @@
+class profile::jenkins {
+
+  class { '::java':
+    version => 'java-1.8.0-openjdk-devel',
+  }
+
+  require ::jenkins
+
+  File {
+    ensure  => file,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode    => '0755',
+    require => Class['jenkins::package'],
+    before  => Class['jenkins::service'],
+  }
+
+  file { '/var/lib/jenkins/jenkins.install.UpgradeWizard.state':
+    content => '2.0',
+  }
+
+  file { '/var/lib/jenkins/secrets/slave-to-master-security-kill-switch':
+    content => 'false',
+  }
+
+  file { '/var/lib/jenkins/init.groovy.d/basic-security.groovy':
+    source => 'puppet:///modules/profile/jenkins_install',
+  }
+
+  include ::git
+}
