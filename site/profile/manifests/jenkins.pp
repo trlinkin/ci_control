7dc7d75413bae91d9c5e2699f7159e66ac5c07a9
Thomas Linkin <trlinkin@gmail.com>
2017-05-23 17:30:12 -0400

    remove Jenkins dep cycle

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -11,7 +11,7 @@ class profile::jenkins {
     owner   => 'jenkins',
     group   => 'jenkins',
     mode    => '0755',
-    require => Class['jenkins::package'],
+    #    require => Class['jenkins::package'],
     before  => Class['jenkins::service'],
   }
 
